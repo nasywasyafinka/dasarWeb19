58a3a3f0d32ba39fc8978fdaf1f5ffa806e0bc7e
Nasywa Syafinka Widyamara <syafinkaforcolle@gmail.com>
2024-04-08 15:12:43 +0700

    pertemuan 8-praktikum 4

--- pertemuan 8-PHP FORM UPLOAD/upload.js
+++ pertemuan 8-PHP FORM UPLOAD/upload.js
@@ -1,12 +1,66 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
+//     $('#upload-form').submit(function (e) {
+//         e.preventDefault();
+//         var formData = new FormData(this);
+
+//         $.ajax({
+//             type: 'POST',
+//             url: 'upload_ajax.php',
+//             data: formData,
+//             cache: false,
+//             contentType: false,
+//             processData: false,
+//             success: function (response) {
+//                 $('#status').html(response);
+//             },
+//             error: function () {
+//                 $('#status').html('Terjadi kesalahan saat menggunggah file.');
+//             }
+//         });
+//     });
+// })
+
+// $(document).ready(function () {
+
+//     $('#upload-form').submit(function (e) {
+//         e.preventDefault();
+//         var formData = new FormData(this);
+
+//         $.ajax({
+//             type: 'POST',
+//             url: 'upload_ajax_gambar.php',
+//             data: formData,
+//             cache: false,
+//             contentType: false,
+//             processData: false,
+//             success: function (response) {
+//                 $('#status').html(response);
+//             },
+//             error: function () {
+//                 $('#status').html('Terjadi kesalahan saat menggunggah file.');
+//             }
+//         });
+//     });
+// })
+
+
+$(document).ready(function () {
+    $('#file').change(function () {
+        if (this.files.length > 0) {
+            $('#upload-button').prop('disabled', false).css('opacity', 1);
+        } else {
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+        }
+    });
     $('#upload-form').submit(function (e) {
         e.preventDefault();
+
         var formData = new FormData(this);
 
         $.ajax({
             type: 'POST',
-            url: 'upload_ajax_gambar.php',
+            url: 'upload_ajax.php',
             data: formData,
             cache: false,
             contentType: false,
