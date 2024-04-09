0930e2b4a6c623b609ead729b4ad089321cc71d2
Nasywa Syafinka Widyamara <syafinkaforcolle@gmail.com>
2024-04-09 23:00:34 +0700

    pertemuan 9-praktikum 2

--- pertemuan 9-LOGIN COOKIES SESSION/query1.3.php
+++
@@ -1,4 +0,0 @@
-<?php
-$connect = mysqli_connect("localhost", "root", "", "prakwebdb");
-$input = mysqli_query($con, "INSERT INTO `user` (`id`, `username
-password) VALUES ('1', 'admin', MD5('123'));");
