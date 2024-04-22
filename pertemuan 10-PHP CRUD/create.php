576b870cf6a4d4ef959f82edc1d11deb7ba9c861
Nasywa Syafinka Widyamara <syafinkaforcolle@gmail.com>
2024-04-22 14:55:46 +0700

    pertemuan 10-praktikum 3

--- pertemuan 10-PHP CRUD/create.php
+++ pertemuan 10-PHP CRUD/create.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Data Anggota</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -13,19 +12,17 @@
         <form action="proses.php?aksi=tambah" method="post">
             <label for="nama">Nama:</label>
             <input type="text" name="nama" id="nama" required>
-            <label for="jenis_kelamin">Jenis Kelamin:</label>
+            <label for="jenis_kelamin">Jenis Kelamin: </label>
             <div class="radio-group">
-                <input type="radio" name="jenis_kelamin" id="laki">
-                <label for="laki">Laki-laki</label>
-                <input type="radio" name="jenis_kelamin" id="perempuan">
-                <label for="perempuan">Perempuan</label>
+                <input type="radio" name="jenis_kelamin" value="L" id="laki" required> <label for="laki">Laki-laki</label>
+                <input type="radio" name="jenis_kelamin" value="p" id="perempuan" required> <label for="perempuan">Perempuan</label>
             </div>
-            <label for="alamat">Alamat</label>
+            <label for="alamat">Alamat:</label>
             <input type="text" name="alamat" id="alamat" required>
-            <label for="no_telp">No. Telp</label>
+            <label for="no_telp">No. Telp: </label>
             <input type="text" name="no_telp" id="no_telp" required>
             <button type="submit">Simpan Data</button>
-            <a href="indeks.php" class="btn-kembali">Kembali</a>
+            <a href="index.php" class="btn-kembali">Kembali</a>
         </form>
     </div>
 </body>
